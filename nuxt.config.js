1943bf57a2c768041b81b3266b86c40edd6cd9b9
jek <jakazzz19@gmail.com>
2021-03-29 00:00:16 +0700

    first

---
+++ nuxt.config.js
@@ -0,0 +1,56 @@
+export default {
+  // Global page headers: https://go.nuxtjs.dev/config-head
+  ssr: true,
+  head: {
+    title: 'Bageur Nuxt',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Bageur Nuxtjs boilerplate' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
+  server: {
+    port: 3456,
+  },
+  // Global CSS: https://go.nuxtjs.dev/config-css
+  css: [
+  ],
+
+  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+  plugins: [
+    // { src: '~/plugins/plugin' , ssr: false},
+  ],
+
+  // Auto import components: https://go.nuxtjs.dev/config-components
+  components: true,
+
+  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+  buildModules: [],
+
+  // Modules: https://go.nuxtjs.dev/config-modules
+  modules: [
+    '@nuxtjs/axios',
+    '@nuxtjs/pwa',
+    ['nuxt-buefy', {  
+      // materialDesignIcons: false 
+    }],
+  ],
+
+  // Axios module configuration: https://go.nuxtjs.dev/config-axios
+  axios: {},
+
+  // PWA module configuration: https://go.nuxtjs.dev/pwa
+  pwa: {
+    manifest: {
+      lang: 'en'
+    }
+  },
+
+  // Build Configuration: https://go.nuxtjs.dev/config-build
+  build: {
+  }
+}
